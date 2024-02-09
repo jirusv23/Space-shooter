eda8e00c6d842c73a4fc502ada4aed06ab7b8673
Vojtěch Jiruš <jirusv23@spstrutnov.cz>
2024-02-09 09:26:39 +0100

    Přidání teleportace mezi kraji, změna rychlosti nepřátel

--- src/SpaceShooter.py
+++ src/SpaceShooter.py
@@ -15,41 +15,50 @@ poziceRaketkyX = 150
 rychlostRaketky = 5
 
 barvaPozadí = (0,0,0)
- #zaklad
+
+pocetNepratel = 3
+ 
+#zaklad
 okno = pygame.display.set_mode(rozliseniObrazovky)
 pygame.display.set_caption("Space Shooter")
 
 #Nepřátelé
 
-class Nepritel:
-    def __init__(self, poziceX, poziceY, vyskaNepratele, sirkaNepratele, rychlostPohybu, rychlostStrelby, rychlostKulky, barvaNepratel):
+class NepritelClass:
+    def __init__(self, poziceX, poziceY, vyskaNepratele, sirkaNepratele, rychlostStrelby, rychlostKulky, barvaNepratel, existuje):
         self.poziceX = poziceX
         self.poziceY = random.randint(0,(vyskaObrazovky-vyskaNepratele))
         
         self.vyskaNepratele = vyskaNepratele
         self.sirkaNepratele = sirkaNepratele
         
-        self.rychlostPohybu = rychlostPohybu
+        self.rychlostPohybu = sirkaNepratele/vyskaNepratele # větší rychlost čím tenší je
         self.rychlostStrelby = rychlostStrelby
         self.rychlostKulky = rychlostKulky
-        self.barvaNepratel = barvaNepratel
-        
+        self.barvaNepratel = barvaNepratel 
+        self.existuje = existuje
+
     def vykresleniNepratel(self):
         pygame.draw.rect(okno, self.barvaNepratel, (self.poziceX, self.poziceY, self.sirkaNepratele, self.vyskaNepratele))
 
+    def PohybNepratel(self):
+        self.poziceX -= self.rychlostPohybu
+    
+    
 Nepratele = []
 
-for i in range(3):
+for i in range(pocetNepratel): 
     Nepratele.append(
-        Nepritel(
-            (sirkaObrazovky - 25), #poziceX
-            random.randint(0,vyskaObrazovky), #poziceY
-            random.randint(0,25), # VýškaNepřátel
-            random.randint(0,25), # ŠířkaNepřátel
-            random.uniform(0.5,1.5), #rychlostPohybu
+        NepritelClass(
+            (sirkaObrazovky + random.randint(25,100)), #poziceX - posune je za obrazovku o nahodnou hodnotu
+            random.randint(10,(vyskaObrazovky - 10)), #poziceY
+            random.randint(25,50), # VýškaNepřátel
+            random.randint(25,100), # ŠířkaNepřátel
+            
             2, #Rychlost Střelby
             2, #Rychlost Kulky
-            (0,0,255) #barvaNepřítele)
+            (0,0,255), #barvaNepřítele
+            True
     ))
 
 
@@ -77,12 +86,18 @@ while run:
     if stisknuteKlavesy[pygame.K_DOWN]: #POHYB DOLŮ
         poziceRaketkyY = poziceRaketkyY + rychlostRaketky
     
+    if poziceRaketkyY <= 0: #Ochrana horniho kraje
+        poziceRaketkyY = vyskaObrazovky - vyskaRaketky 
+    elif poziceRaketkyY >= vyskaObrazovky - vyskaRaketky:
+        poziceRaketkyY = 0
     
+    for NepritelClass in Nepratele:
+        NepritelClass.PohybNepratel()
     
     okno.fill(barvaPozadí)
     
-    for Nepritel in Nepratele:
-        Nepritel.vykresleniNepratel()
-    
+    for NepritelClass in Nepratele:
+        NepritelClass.vykresleniNepratel()
+        
     pygame.draw.rect(okno, barvaRaketky, (poziceRaketkyX, poziceRaketkyY, sirkaRaketky, vyskaRaketky))
     pygame.display.update() 
